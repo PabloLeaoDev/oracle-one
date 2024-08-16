9afa5e3f9e75d48fa63571fe783d8b12937b7823
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2024-08-16 13:16:33 -0300

    decodificador v1 finalizada

--- decodificador/src/main.js
+++ decodificador/src/main.js
@@ -1,31 +1,48 @@
 const VOWELS = ['a', 'e', 'i', 'o', 'u'];
 const KEYS = ['ai', 'enter', 'imes', 'ober', 'ufat'];
 let index;
+let flagEncrypt = false;
+let valueText = '';
 
 function encrypter() {
-    const removeTxt = document.querySelector('#non-message');
-    removeTxt.classList.add('desactive');
+    if (!flagEncrypt) {
+        const removeTxt = document.querySelector('#non-message');
+        removeTxt.classList.add('desactive');
 
-    const btnCopy = document.querySelector('#copy');
-    btnCopy.classList.add('active');
+        const btnCopy = document.querySelector('#copy');
+        btnCopy.classList.add('active');
 
-    const answer = document.querySelector('#answer-p');
-    const txt = document.querySelector('#txt');
-    const phrase = txt.value;
-    const encryptTxt = encrypt(phrase);
-    answer.innerHTML = encryptTxt;
+        const answer = document.querySelector('#answer-p');
+        const txt = document.querySelector('#txt');
+        answer.classList.add('answer');
+
+        const phrase = txt.value;
+        const encryptTxt = encrypt(phrase);
+        answer.innerHTML = encryptTxt;
+        flagEncrypt = !flagEncrypt;
+        valueText = txt.value;
+    } else if (flagEncrypt && txt.value === valueText) {
+        window.alert('O texto já foi criptografado!');
+    } else {
+        window.alert('Não posso criptografar um texto que já foi criptografado!');
+    }
 }
 
 function descrypter() {
-    const removeTxt = document.querySelector('#non-message');
-    removeTxt.classList.add('desactive');
-
-    const btnCopy = document.querySelector('#copy');
-    btnCopy.classList.add('active');
-
-    const answer = document.querySelector('#answer-p');
     const txt = document.querySelector('#txt');
-    const phrase = txt.value;
-    const descryptTxt = descrypt(phrase);
-    answer.innerHTML = descryptTxt;
+    if (flagEncrypt && txt.value != valueText) {
+        const removeTxt = document.querySelector('#non-message');
+        removeTxt.classList.add('desactive');
+
+        const btnCopy = document.querySelector('#copy');
+        btnCopy.classList.add('active');
+
+        const answer = document.querySelector('#answer-p');
+        const phrase = txt.value;
+        const descryptTxt = descrypt(phrase);
+        answer.innerHTML = descryptTxt;
+        flagEncrypt = !flagEncrypt;
+    } else {
+        window.alert('Por favor, criptografe a frase antes de descriptografar!');
+    }
 }
